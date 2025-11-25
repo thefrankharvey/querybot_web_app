74384ca8d2c2fca8692c9f89705659fdcfd0ec5e
matthew-garrett <matthew.garrett252@gmail.com>
2025-11-25 15:14:11 -0500

    Fix build error

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import CopyToClipboard from "./copy-to-clipboard";
-import Image from "next/image";
 import Link from "next/link";
 import { SignedOut, SignUpButton } from "@clerk/nextjs";
 
