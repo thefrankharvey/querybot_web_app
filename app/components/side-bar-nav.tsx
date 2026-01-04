fbf962689a5ec4ddf1385a97792414b075dcca0d
matthew-garrett <matthew.garrett252@gmail.com>
2026-01-04 08:40:02 -0500

    Rebase clean up

--- app/components/side-bar-nav.tsx
+++ app/components/side-bar-nav.tsx
@@ -51,17 +51,17 @@ export const SideBarNav = () => {
   };
 
   return (
-    <div className="w-[180px] shrink-0 pt-12 ml-8 sticky top-0 self-start h-fit hidden md:block">
+    <div className="w-[180px] shrink-0 pt-4 ml-8 sticky top-0 self-start h-fit hidden md:block">
       <Link href="/" className="text-xl font-semibold text-black">
         <Image
           src="/wqh-logo.png"
           alt="logo"
           width={60}
           height={60}
-          className="w-[60px] h-[60px] rounded-full"
+          className="w-[60px] h-[60px] rounded-full ml-8"
         />
       </Link>
-      <div className="w-full flex flex-col md:flex-row">
+      <div className="w-full flex flex-col md:flex-row pt-8">
         <aside className="w-full md:sticky md:top-24 h-full md:max-w-[180px]">
           <nav className="w-full flex flex-col gap-2 p-4 md:p-0 rounded-none shadow-none mt-0 md:mt-16">
             {!isSubscribed && (
